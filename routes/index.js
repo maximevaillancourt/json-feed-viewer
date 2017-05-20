f7a1151bb90318aae37a95cf9d37e293d654facf
Maxime Vaillancourt <maxvaillancourt1@gmail.com>
2017-05-20 11:00:03 -0400

    added 'jordan merrick' feed

--- routes/index.js
+++ routes/index.js
@@ -42,6 +42,7 @@ router.get('/',
         "https://longstride.net/feed.json",
         "https://ryanmo.co/feed.json",
         "http://troz.net/feed.json",
+        "https://www.jordanmerrick.com/feed.json",
         //"https://pxlnv.com/feed/json/",
         //"http://uncrate.com/feed.json",
         //"http://www.vienna-rb.at/new-blog/feed.json"
